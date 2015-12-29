7970fc4397f295e3a290d9748c68434852661bac
cesar <cesar@romero.ws>
2015-12-29 16:31:01 -0430

    better random board init

--- triplecross.py
+++ triplecross.py
@@ -36,9 +36,20 @@ class TripleCrossBoard(object):
 
         return ans
 
+    def __getitem__(self, key):
+        return self.tiles[key]
+
     @staticmethod
     def random_tiles(nrows = 2, ncols = 7):
-        return [[random.sample('_dabqezcx', 1)[0] for c in range(ncols)] for r in range(nrows)]
+        ans = [['_' for c in range(ncols)] for r in range(nrows)]
+        pop = list(range(nrows*ncols))
+        tiles = random.sample(pop, 9)
+        symbols = 'dddabqezc'
+        for tile, symbol in zip(tiles, symbols):
+            row = tile // ncols
+            col = tile % ncols
+            ans[row][col] = symbol
+        return ans
 
 class TripleCross(object):
 
@@ -46,16 +57,41 @@ class TripleCross(object):
     RIGHT = 'right'
     UP = 'up'
     DOWN = 'down'
+    DOME_UL = 'q'
+    DOME_UR = 'e'
+    DOME_LL = 'z'
+    DOME_LR = 'c'
 
     def __init__(self, nrows = 4, ncols = 9):
+        self.nrows = nrows
+        self.ncols = ncols
         self.board = TripleCrossBoard()
 
     def __str__(self):
         return str(self.board)
 
+    def goal(self):
+        '''Returns True when the current state corresponds to one of the valid
+        goals defined for the TripleCross game.
+        '''
+        return self._dome() or self._dots() or self._binarts()
+
     def move(self, op):
         if op not in [UP, RIGHT, LEFT, DOWN]:
             raise Exception('invalid operation: ', op)
+
+    def _dome(self):
+        row,col = index(DOME_UL)
+        return row != 0 and col != self.ncols - 1 \
+        and self.board[row][col+1] == DOME_UR \
+        and self.board[row+1][col+1] == DOME_LR \
+        and self.board[row+1][col-1] == DOME_LL
+
+    def _dots(self):
+        return false
+
+    def _binarts(self):
+        return false
 
 if __name__ == '__main__':
     print(TripleCrossBoard(up=False, offset=1))
